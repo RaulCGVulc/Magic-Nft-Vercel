2ee4413fe92c0e0dbd0154ca742b6e43e4017515
RaulCGVulc <raul@vulcanics.mx>
2023-08-04 12:19:47 -0600

    cambiando logica y codigos de respuesta http

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -1,29 +1,28 @@
 import { transporter, mailOptions } from '../../configs/nodemailer';
 
 export default async function sendEmail(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body
-        if (!data.wallet || !data.balance.toString() || !data.twitter) {
-            return res.status(400).json({ message: 'campos necesarios no encontrados' })
-        }
-        const text = JSON.stringify(data)
+    console.log(req.method, req.body);
 
-        try {
-            const response = await transporter.sendMail({
+    if (req.method === 'POST') {
+        const data = req.body;
+        console.log(data);
+
+        if (!data.pubKey || !data.balance.toString() || !data.twitter) {
+            return res.status(401).json({ message: 'campos necesarios no encontrados' })
+        } else {
+            const text = JSON.stringify(data)
+            
+            await transporter.sendMail({
                 ...mailOptions,
                 subject: 'magic money ',
                 text,
+            }).then((response) => {
+                return res.status(201).json({ response })
             })
-
-
-            res.status(201).json({ response })
-        } catch (err) {
-            console.log(err)
-            return res.status(400).json({ message: err })
         }
-
+    } else {
+        return res.status(402).json({ message: 'peticion incorrecta' })
     }
-
-    return res.status(400).json({ message: 'peticion incorrecta' })
 }
+
 
